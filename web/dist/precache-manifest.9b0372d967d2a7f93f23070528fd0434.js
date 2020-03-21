28aec61de0bdd243fe88ee44ab5274ae91766e7d
Sagnik Modak <sagnikmodak1118@gmail.com>
2020-03-21 18:20:47 +0530

    feat: web

---
+++ web/dist/precache-manifest.9b0372d967d2a7f93f23070528fd0434.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a282fea4100e48e30894",
+    "url": "/css/chunk-vendors.2b46f915.css"
+  },
+  {
+    "revision": "63d2a5950fc212096c3612f71ae66f64",
+    "url": "/fonts/materialdesignicons-webfont.63d2a595.woff"
+  },
+  {
+    "revision": "7fb0e3780372f10f804513e2e0c39e78",
+    "url": "/fonts/materialdesignicons-webfont.7fb0e378.woff2"
+  },
+  {
+    "revision": "a1a0ed860f50ffd42ba46d7c10f30255",
+    "url": "/fonts/materialdesignicons-webfont.a1a0ed86.eot"
+  },
+  {
+    "revision": "fe1545ef4dd1eef2f1e25528898fc0b3",
+    "url": "/fonts/materialdesignicons-webfont.fe1545ef.ttf"
+  },
+  {
+    "revision": "b74f7557e07951d9a62731b3cd644aa0",
+    "url": "/img/marvin-message.b74f7557.png"
+  },
+  {
+    "revision": "173c2a06deac7b224e8ff62abfb8bc5e",
+    "url": "/img/marvin.173c2a06.png"
+  },
+  {
+    "revision": "ffa842a607b27505f25e631190168dcb",
+    "url": "/img/proxy-bg.ffa842a6.jpg"
+  },
+  {
+    "revision": "d8206d1e34c1b0f92949701a579f53b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b69294238fecb5f44e3b",
+    "url": "/js/app.2f974dff.js"
+  },
+  {
+    "revision": "ad3c3a05b24fd215352f",
+    "url": "/js/chunk-2d22d746.a87311a3.js"
+  },
+  {
+    "revision": "a282fea4100e48e30894",
+    "url": "/js/chunk-vendors.b885c4ca.js"
+  },
+  {
+    "revision": "ef396125f3b530e82478cf12b1e12bd2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
